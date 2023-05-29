c8315a6fc3afb242ef9ae05c797ceba3d22c0a1f
Sergio Donini <117519910+sdonini@users.noreply.github.com>
2023-05-29 19:15:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao banco de dados')
+console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O Paulo fez esse commit sem voce ver na aula!")
 console.log("Enviando commit para o github")
